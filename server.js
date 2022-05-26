7664adb4134ae785eebf64c0fe008256044246cf
Camille Salikhov <dontlookbackinanger@yandex.ru>
2022-05-26 22:15:03 +0300

    another heroku commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log('running'));
+app.listen(PORT,"0.0.0.0", console.log('running'));
 
 connectDB();
 
